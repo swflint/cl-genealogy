7027c8ec33b5375c4b0a97faeb1f3e629cf8a980
Samuel W. Flint <swflint@flintfam.org>
2015-03-30 12:18:29 -0500

    Added some stuff to routes

--- routes.lisp
+++ routes.lisp
@@ -27,22 +27,74 @@
 (define-route main ("")
   (main-page :nav (generate-nav :main)))
 
-(define-route add ("add"))
+(define-route add ("add")
+  (main-page :nav (generate-nav :main)
+             :title "Add"
+             :content
+             (with-html-output-to-string (out)
+               (:dl
+                (:dt (:a :href (str (genurl 'add/person)) "Add Person"))
+                (:dd "Add a person to the genealogical database.")
+                (:dt (:a :href (str (genurl 'add/death)) "Add Death"))
+                (:dd "Add a death record to the genealogical database.")
+                (:dt (:a :href (str (genurl 'add/marriage)) "Add Marriage"))
+                (:dd "Add a marriage record to the genealogical database.")
+                (:dt (:a :href (str (genurl 'add/divorce)) "Add Divorce"))
+                (:dd "Add a divorce record to the genealogical database")))))
 (define-route add/person ("add/person"))
+(define-route add/person/post ("add/person" :method :post))
 (define-route add/marriage ("add/marriage"))
 (define-route add/divorce ("add/divorce"))
 (define-route add/death ("add/death"))
 
-(define-route notes ("notes"))
+(define-route notes ("notes")
+  (main-page :title "Notes Management"
+             :nav (generate-nav :main)
+             :content
+             (with-html-output-to-string (output)
+               (:dl
+                (:dt (:a :href (str (genurl 'notes/add)) "Add Note"))
+                (:dd "Add a research note.")
+                (:dt (:a :href (str (genurl 'notes/view)) "View Notes"))
+                (:dd "View research notes.")
+                (:dt (:a :href (str (genurl 'notes/edit)) "Edit Note"))
+                (:dd "Select and edit research notes.")))))
 (define-route notes/add ("notes/add"))
 (define-route notes/edit ("notes/edit"))
 (define-route notes/view ("notes/view"))
 
-(define-route records ("records"))
+(define-route records ("records")
+  (main-page :title "Records Management"
+             :nav (generate-nav :main)
+             :content
+             (with-html-output-to-string (output)
+               (:dl
+                (:dt (:a :href (str (genurl 'records/add)) "Add Record"))
+                (:dd "Add a copy of a historical record/citation.")
+                (:dt (:a :href (str (genurl 'records/view)) "View Records"))
+                (:dd "View records/citations.")))))
 (define-route records/add ("records/add"))
 (define-route records/view ("records/view"))
 
-(define-route reports ("reports"))
+(define-route reports ("reports")
+  (main-page :title "Reports"
+             :nav (generate-nav :main)
+             :content
+             (with-html-output-to-string (output)
+               (:dl
+                (:dt (:a :href (str (genurl 'reports/add)) "Add Report"))
+                (:dd "Add a report of one of the following types:"
+                     (:ul
+                      (:li "Family Tree")
+                      (:li "Ahnentafel")
+                      (:li "Biography")
+                      (:li "Complete genealogical report")))
+                (:dt (:a :href (str (genurl 'reports/view)) "View Report"))
+                (:dd "View report properties/report.")
+                (:dt (:a :href (str (genurl 'reports/edit)) "Edit Report"))
+                (:dd "Edit report properties/report content.")
+                (:dt (:a :href (str (genurl 'reports/generate)) "Generate Report"))
+                (:dd "Generate formatted report output.")))))
 (define-route reports/add ("reports/add"))
 (define-route reports/view ("reports/view"))
 (define-route reports/edit ("reports/edit"))
