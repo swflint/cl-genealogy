c2c458fb00396d33dd1096d67422664e2cf336a5
Samuel W. Flint <swflint@flintfam.org>
2016-03-04 20:55:42 -0600

    Moved to genie.lisp

--- genie.lisp
+++ genie.lisp
@@ -2,7 +2,26 @@
 ;;;;
 ;;;; Copyright (c) 2015 Samuel Flint <swflint@lisp.technology>
 
+(restas:define-module #:genie
+  (:use #:cl
+        #:lambdalite
+        #:config-parser
+        #:restas
+        #:cl-who
+        #:iterate)
+  (:import-from #:ironclad
+                #:pbkdf2-hash-password
+                #:byte-array-to-hex-string
+                #:hex-string-to-byte-array)
+  (:import-from #:hunchentoot
+                #:post-parameter
+                #:start-session
+                #:session-value)
+  (:import-from #:html-template
+                #:create-template-printer)
+  (:import-from #:lass
+                #:compile-and-write))
+
 (in-package #:genie)
 
 ;;; "genie" goes here. Hacks and glory await!
-
--- package.lisp
+++ package.lisp
@@ -20,23 +20,3 @@
            new-death
            new-marriage
            new-divorce))
-
-(restas:define-module #:genie
-  (:use #:cl
-        #:lambdalite
-        #:config-parser
-        #:restas
-        #:cl-who
-        #:iterate)
-  (:import-from #:ironclad
-                #:pbkdf2-hash-password
-                #:byte-array-to-hex-string
-                #:hex-string-to-byte-array)
-  (:import-from #:hunchentoot
-                #:post-parameter
-                #:start-session
-                #:session-value)
-  (:import-from #:html-template
-                #:create-template-printer)
-  (:import-from #:lass
-                #:compile-and-write))
